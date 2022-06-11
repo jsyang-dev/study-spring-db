c6c50d97a0c7a19fd73e635e6a5cef5dad664c0b
Jeongsu Yang <mycat83@gmail.com>
2022-06-11 22:34:59 +0900

    feat: JDBC 개발 - 조회

--- src/test/java/me/study/springdb/repository/MemberRepositoryV0Test.java
+++ src/test/java/me/study/springdb/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,27 @@
 package me.study.springdb.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import me.study.springdb.domain.Member;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        // save
         final Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        // findById
+        final Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
