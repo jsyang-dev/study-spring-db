174a4d0ade55c452179991499fbfbb05ad0df8cf
Jeongsu Yang <mycat83@gmail.com>
2022-06-14 15:49:48 +0900

    feat: DataSource 예제2 - 커넥션 풀

--- src/test/java/me/study/springdb/connection/ConnectionTest.java
+++ src/test/java/me/study/springdb/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package me.study.springdb.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -23,9 +24,22 @@ public class ConnectionTest {
     }
 
     @Test
-    void driverManagerDataSource() throws SQLException {
+    void dataSourceDriverManager() throws SQLException {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         userDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        final HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void userDataSource(DataSource dataSource) throws SQLException {
