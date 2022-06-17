95efb2d5550651c15878d3f028c6a5fb2d44c817
Jeongsu Yang <mycat83@gmail.com>
2022-06-17 13:57:51 +0900

    feat: 트랜잭션 - 적용2

--- src/test/java/me/study/springdb/service/MemberServiceV2Test.java
+++ src/test/java/me/study/springdb/service/MemberServiceV2Test.java
@@ -1,7 +1,7 @@
 package me.study.springdb.service;
 
 import me.study.springdb.domain.Member;
-import me.study.springdb.repository.MemberRepositoryV1;
+import me.study.springdb.repository.MemberRepositoryV2;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,22 +14,22 @@ import static me.study.springdb.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.*;
 
 /**
- * 기본 동작, 트랜잭션이 없어서 문제 발생
+ * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
  */
-class MemberServiceV1Test {
+class MemberServiceV2Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
-    private MemberRepositoryV1 memberRepository;
-    private MemberServiceV1 memberService;
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
 
     @BeforeEach
     void setUp() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV1(dataSource);
-        memberService = new MemberServiceV1(memberRepository);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
     }
 
     @AfterEach
@@ -74,7 +74,7 @@ class MemberServiceV1Test {
         // then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberB = memberRepository.findById(memberEx.getMemberId());
-        assertThat(findMemberA.getMoney()).isEqualTo(8_000);
+        assertThat(findMemberA.getMoney()).isEqualTo(10_000);
         assertThat(findMemberB.getMoney()).isEqualTo(10_000);
     }
 }
